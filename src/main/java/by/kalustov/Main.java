98230347777ae724bd7529956af4cdc29e1479d4
jskov <jskov259259@rambler.ru>
2023-02-15 23:17:42 +0300

    feat: working all tasks

--- src/main/java/by/kalustov/Main.java
+++ src/main/java/by/kalustov/Main.java
@@ -12,31 +12,37 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.*;
+import java.util.concurrent.Flow;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        task1();
-//        task2();
-//        task3();
-//        task4();
-//        task5();
-//        task6();
-//        task7();
-//        task8();
-//        task9();
-//        task10();
-//        task11();
-//        task12();
-//        task13();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+        task11();
+        task12();
+        task13();
         task14();
         task15();
+        task16();
     }
 
+
     private static void task1() throws IOException {
+        System.out.println("task1");
         List<Animal> animals = Util.getAnimals();
                 animals.stream().filter(animal -> animal.getAge() >= 10 && animal.getAge() <= 20)
                         .sorted(Comparator.comparing(Animal::getAge))
@@ -47,6 +53,7 @@ public class Main {
     }
 
     private static void task2() throws IOException {
+        System.out.println("task2");
         List<Animal> animals = Util.getAnimals();
                 animals.stream().filter(animal -> animal.getOrigin().equals("Japanese"))
                         .map(animal -> {
@@ -60,6 +67,7 @@ public class Main {
     }
 
     private static void task3() throws IOException {
+        System.out.println("task3");
         List<Animal> animals = Util.getAnimals();
                 animals.stream().filter(animal -> animal.getAge() >= 30 && animal.getOrigin().startsWith("A"))
                         .map(Animal::getOrigin)
@@ -69,6 +77,7 @@ public class Main {
     }
 
     private static void task4() throws IOException {
+        System.out.println("task4");
         List<Animal> animals = Util.getAnimals();
                 long count = animals.stream().filter(animal -> "Female".equals(animal.getGender()))
                         .count();
@@ -76,6 +85,7 @@ public class Main {
     }
 
     private static void task5() throws IOException {
+        System.out.println("task5");
         List<Animal> animals = Util.getAnimals();
                boolean isAnyHungarian = animals.stream().filter(animal -> animal.getAge() >= 10 && animal.getAge() <= 20)
                         .anyMatch(animal -> "Hungarian".equals(animal.getOrigin()));
@@ -83,6 +93,7 @@ public class Main {
     }
 
     private static void task6() throws IOException {
+        System.out.println("task6");
         List<Animal> animals = Util.getAnimals();
                boolean isAllMaleAndFemale = animals.stream().allMatch(animal -> "Female".equals(animal.getGender())
                        || "Male".equals(animal.getGender()));
@@ -90,12 +101,14 @@ public class Main {
     }
 
     private static void task7() throws IOException {
+        System.out.println("task7");
         List<Animal> animals = Util.getAnimals();
                boolean isNoneOceania = animals.stream().noneMatch(animal -> "Oceania".equals(animal.getOrigin()));
         System.out.println(isNoneOceania);
     }
 
     private static void task8() throws IOException {
+        System.out.println("task8");
         List<Animal> animals = Util.getAnimals();
                Optional<Integer> oldestAge = animals.stream().sorted(Comparator.comparing(Animal::getBread))
                         .limit(100)
@@ -106,6 +119,7 @@ public class Main {
     }
 
     private static void task9() throws IOException {
+        System.out.println("task9");
         List<Animal> animals = Util.getAnimals();
         Optional<char[]> minLength = animals.stream().flatMap(animal -> {
                     char[] bread = animal.getBread().toCharArray();
@@ -115,12 +129,14 @@ public class Main {
     }
 
     private static void task10() throws IOException {
+        System.out.println("task10");
         List<Animal> animals = Util.getAnimals();
             Integer ageSum = animals.stream().map(Animal::getAge).reduce(0, Integer::sum);
             System.out.println(ageSum);
     }
 
     private static void task11() throws IOException {
+        System.out.println("task11");
         List<Animal> animals = Util.getAnimals();
                OptionalDouble average = animals.stream().filter(animal -> "Indonesian".equals(animal.getOrigin()))
                        .mapToInt(Animal::getAge)
@@ -129,6 +145,7 @@ public class Main {
     }
 
     private static void task12() throws IOException {
+        System.out.println("task12");
         List<Person> people = Util.getPersons();
         people.stream().filter(person -> {
             Integer age = Period.between(person.getDateOfBirth(), LocalDate.now()).getYears();
@@ -141,6 +158,7 @@ public class Main {
     }
 
     private static void task13() throws IOException {
+        System.out.println("task13");
         List<House> houses = Util.getHouses();
         List<Person> patients = new ArrayList<>();
         List<Person> youngsAndOlds = new ArrayList<>();
@@ -178,34 +196,117 @@ public class Main {
     }
 
     private static void task14() throws IOException {
+        System.out.println("task14");
         List<Car> cars = Util.getCars();
-        AtomicReference<BigDecimal> revenue = new AtomicReference<>(new BigDecimal("0"));
 
-        int tm_mass = cars.stream().filter(car -> "Jaguar".equals(car.getCarMake()) || "White".equals(car.getColor()))
-                .peek(System.out::println)
-                       .reduce(0, (sum, car) -> {
-                    BigDecimal expense = BigDecimal.valueOf(car.getMass()*7.14/1000);
-                    revenue.getAndUpdate(x -> x.add(expense));
-                    return sum + car.getMass();
-                }, (sum, car) -> sum + car);
-        Stream<Car> uz_stream = cars.stream().filter(car -> car.getMass() <= 1500 || "BMW".equals(car.getCarModel())
-                        || "Lexus".equals(car.getCarModel()) || "Chrysler".equals(car.getCarModel())
-                        || "Toyota".equals(car.getCarModel()));
-        Stream<Car> kz_stream = cars.stream().filter(car -> ("Black".equals(car.getColor()) && car.getMass() >= 4000)
-                        || "GMC".equals(car.getCarModel()) || "Dodge".equals(car.getCarModel()));
-        Stream<Car> kg_stream = cars.stream().filter(car -> car.getReleaseYear() < 1982 || "Civic".equals(car.getCarModel())
-                        || "Cherokee".equals(car.getCarModel()));
-        Stream<Car> ru_stream = cars.stream().filter(car -> !"Yellow".equals(car.getColor()) || !"Red".equals(car.getColor())
-                || !"Green".equals(car.getColor()) || !"Blue".equals(car.getColor()) || car.getPrice() > 40000);
-        Stream<Car> mn_stream = cars.stream().filter(car -> car.getVin().contains("59"));
+        BiFunction<Integer, Car, Integer> countryMassFunction = (sum, car) -> sum + car.getMass();
 
-        System.out.println(tm_mass);
-        System.out.println(revenue.get());
+        Predicate<Car> tmPredicate = car -> "Jaguar".equals(car.getCarMake()) || "White".equals(car.getColor());
+        Predicate<Car> uzPredicate = car -> car.getMass() <= 1500 || "BMW".equals(car.getCarMake())
+                || "Lexus".equals(car.getCarMake()) || "Chrysler".equals(car.getCarMake())
+                || "Toyota".equals(car.getCarMake());
+        Predicate<Car> kzPredicate = car -> ("Black".equals(car.getColor()) && car.getMass() >= 4000)
+                || "GMC".equals(car.getCarMake()) || "Dodge".equals(car.getCarMake());
+        Predicate<Car> kgPredicate = car -> car.getReleaseYear() < 1982 || "Civic".equals(car.getCarModel())
+                || "Cherokee".equals(car.getCarModel());
+        Predicate<Car> ruPredicate = car -> !"Yellow".equals(car.getColor()) || !"Red".equals(car.getColor())
+                || !"Green".equals(car.getColor()) || !"Blue".equals(car.getColor()) || car.getPrice() > 40000;
+        Predicate<Car> mnPredicate = car -> car.getVin().contains("59");
+
+        int tmMass = cars.stream().filter(tmPredicate).reduce(0, countryMassFunction, Integer::sum);
+
+        List<Car> uzCars = cars.stream().dropWhile(tmPredicate).collect(Collectors.toList());
+        int uzMass = uzCars.stream().filter(uzPredicate).reduce(0, countryMassFunction, Integer::sum);
+
+        List<Car> kzCars = uzCars.stream().dropWhile(uzPredicate).collect(Collectors.toList());
+        int kzMass = kzCars.stream().filter(kzPredicate).reduce(0, countryMassFunction, Integer::sum);
+
+        List<Car> kgCars = kzCars.stream().dropWhile(kzPredicate).collect(Collectors.toList());
+        int kgMass = kgCars.stream().filter(kgPredicate).reduce(0, countryMassFunction, Integer::sum);
+
+        List<Car> ruCars = kgCars.stream().dropWhile(kgPredicate).collect(Collectors.toList());
+        int ruMass = ruCars.stream().filter(ruPredicate).reduce(0, countryMassFunction, Integer::sum);
+
+        List<Car> mnCars = ruCars.stream().dropWhile(ruPredicate).collect(Collectors.toList());
+        int mnMass = mnCars.stream().filter(mnPredicate).reduce(0, countryMassFunction, Integer::sum);
+
+        IntStream massStream = IntStream.of(tmMass, uzMass, kzMass, kgMass, ruMass, mnMass);
+        AtomicReference<BigDecimal> profit = new AtomicReference<>(new BigDecimal("0"));
+        massStream.forEach(mass -> {
+            BigDecimal expense = BigDecimal.valueOf(mass*7.14/1000);
+            profit.getAndUpdate(x -> x.add(expense));
+            System.out.println("Mass = " + mass + ", expense = " + expense);
+        });
+        System.out.println("Profit = " + profit.get());
 
     }
 
     private static void task15() throws IOException {
+        System.out.println("task15");
         List<Flower> flowers = Util.getFlowers();
-        //        Продолжить...
+
+        Comparator countryComparator = Comparator.comparing(Flower::getOrigin);
+        Comparator priceComparator = Comparator.comparing(Flower::getPrice);
+        Comparator waterConsumptionComparator = Comparator.comparing(Flower::getWaterConsumptionPerDay);
+        Comparator nameComparator = Comparator.comparing(Flower::getCommonName);
+
+        Predicate<Flower> nameFilter = flower -> {
+            char letter = flower.getCommonName().charAt(0);
+            return letter >= 67 && letter <= 83;
+        };
+
+        Predicate<Flower> shadeVaseFilter = flower -> flower.getFlowerVaseMaterial().stream()
+                .anyMatch(material -> "Aluminum".equals(material) || "Glass".equals(material)
+            || "Steel".equals(material)) && flower.isShadePreferred();
+
+
+        Function<Flower, BigDecimal> waterConsumptionFunction = flower -> {
+            BigDecimal waterConsumption = BigDecimal.valueOf(flower.getWaterConsumptionPerDay()/1000*1825*1.39);
+            BigDecimal priceAndConsumption =  waterConsumption.add(BigDecimal.valueOf(flower.getPrice()));
+            return waterConsumption;
+        };
+
+        Stream<BigDecimal> plantCareStream = flowers.stream().filter(nameFilter).filter(shadeVaseFilter).sorted(countryComparator.reversed()
+                .thenComparing(priceComparator.reversed())
+                .thenComparing(waterConsumptionComparator.reversed())
+                .thenComparing(nameComparator.reversed()))
+                .map(waterConsumptionFunction);
+
+        BigDecimal plantCareSum = plantCareStream.reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println("Total cost of care: " + plantCareSum);
+
+    }
+
+    public static void task16() throws IOException {
+        System.out.println("task16");
+        List<Person> people = Util.getPersons();
+        // first task
+        ToIntFunction<Person> averageAgeFunction = person -> Period.between(person.getDateOfBirth(), LocalDate.now()).getYears();
+
+        Map<String, Double> ageByOccupation = people.stream()
+                .collect(Collectors.groupingBy(Person::getOccupation,
+                        Collectors.averagingInt(averageAgeFunction)));
+
+        Predicate<Map.Entry<String, Double>> ageFilter = entry -> entry.getValue() >=20 && entry.getValue() <=25;
+
+        ageByOccupation.entrySet().stream()
+                .filter(ageFilter)
+                .sorted(Map.Entry.<String, Double>comparingByKey().reversed())
+                .forEach(entry -> System.out.println(entry.getKey() + " = " + String.format("%.0f", entry.getValue())));
+
+        System.out.println("-----");
+
+        // second task
+        Function<Person, Integer> monthOfBirthdayFunction = person -> person.getDateOfBirth().getMonthValue();
+        Map<Integer, List<String>> monthByOccupation = people.stream()
+                .collect(Collectors.groupingBy(monthOfBirthdayFunction,
+                        Collectors.mapping(Person::getOccupation, Collectors.toList())));
+
+        monthByOccupation.values().stream().map(list -> {
+            Map<String, Long> occupationCount = list.stream()
+                .collect(Collectors.groupingBy(String::valueOf, Collectors.counting()));
+           return Collections.max(occupationCount.entrySet(), Comparator.comparingLong(Map.Entry::getValue)).getKey();
+        }).forEach(System.out::println);
+
     }
 }
